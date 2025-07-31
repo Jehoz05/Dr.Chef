6084ae6da0ae74463c6e8adfd9e62a8520bc2a2e
Jehoz05 <lkjehoz@gmail.com>
2025-07-31 18:31:55 +0530

    supabase-commit

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 import { notFound } from "next/navigation";
@@ -17,10 +16,15 @@ export async function generateMetadata({ params }) {
   };
 }
 
-export default function MealDetailsPage({ params }) {
-  const meal = getMeal(params.mealSlug);
+export default async function MealDetailsPage({ params }) {
+  const meal = await getMeal(params.mealSlug);
 
-  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  if (!meal) {
+    notFound();
+  }
+
+  const formattedInstructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
     <>
       <header className={classes.header}>
@@ -30,7 +34,7 @@ export default function MealDetailsPage({ params }) {
         <div className={classes.headerText}>
           <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            by <a href={`mailto:${"EMAIL"}`}>{meal.name}</a>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
           <p className={classes.summary}>{meal.summary}</p>
         </div>
@@ -39,7 +43,7 @@ export default function MealDetailsPage({ params }) {
         <p
           className={classes.instructions}
           dangerouslySetInnerHTML={{
-            __html: meal.instructions,
+            __html: formattedInstructions,
           }}
         ></p>
       </main>
